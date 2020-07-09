a7c0f59231bc24f95460761c7da2a8d0caa14717
markzzzzzzz <markuslaidmets@hot.ee>
2020-07-09 16:24:16 +0300

    first-angular

---
+++ 09.07/form-javascript.js
@@ -0,0 +1,54 @@
+
+var registrationsDiv = document.getElementById("registrations");
+console.log(registrationsDiv);
+var registrationsData = "";
+
+function buildForm() {
+    var isEditMode = localStorage.getItem("editmode");
+    if (isEditMode=="true") {
+        document.getElementById("firstname").value = localStorage.getItem("firstname");
+        document.getElementById("lastname").value = localStorage.getItem("lastname");
+        document.getElementById("phone").value = localStorage.getItem("phone");
+        document.getElementById("email").value = localStorage.getItem("email");
+
+    }
+    localStorage.removeItem("editmode");
+}
+
+
+
+function onSubmit(e) {
+    // preventDefault abil ei tule refreshi
+    // e tähendab eventi
+    e.preventDefault();
+    console.log(e);
+    console.log("töötab");
+
+    document.forms["registrationForm"]
+
+    console.log(registrationForm)
+    console.log(registrationForm["firstname"]);
+    console.log(registrationForm["firstname"].value);
+
+    var firstName = registrationForm["firstname"].value;
+    console.log(firstName);
+    var lastName = registrationForm["lastname"].value;
+    console.log(lastName);
+    var number = registrationForm["phone"].value;
+    console.log(number);
+    var email = registrationForm["email"].value;
+    console.log(email);
+
+    registrationForm.reset();
+    
+    //registrationsData = registrationsData + "Eesnimi: " + firstName + "<br>" + "Perekonnanimi: " + lastName +  "<br>" + "Telefon: " + number + "<br>" + "Email: " + email + "<br>";
+    //registrationsDiv.innerHTML = registrationsData;
+
+    localStorage.setItem("firstname", firstName);
+    localStorage.setItem("lastname", lastName);
+    localStorage.setItem("phone", number);
+    localStorage.setItem("email", email);
+
+    location.href = "review.html";
+
+}
