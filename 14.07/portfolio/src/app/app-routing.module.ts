bd99c04565ba4b9a5eda21644fabac13f91af560
markzzzzzzz <markuslaidmets@hot.ee>
2020-07-14 16:23:06 +0300

    portfoolio

---
+++ 14.07/portfolio/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { CoursesComponent } from './courses/courses.component';
+import { WorkComponent } from './work/work.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+
+
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'courses', component: CoursesComponent },
+  { path: 'hobbies', component: HobbiesComponent },
+  { path: 'work', component: WorkComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
